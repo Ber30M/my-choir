374fdaccaf3fe2771b62735b5883ec9cfe9e4edf
Ber30 <makoberobertrand@gmail.com>
2022-11-25 20:03:41 +0200

    ionic app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>URUFATANAMENDE</ion-title>
+        <ion-title>MY FIRST IONIC APP</ion-title>
       </ion-toolbar>
-      <ion-content class="ion-padding">
-        Kaze m'URUFATANAMENDE
+      </ion-header>
+     <ion-content class="ion-padding">
+        <ion-item>
+          <ion-label position="floating">Your name</ion-label>
+          <ion-input clear-input="true" placeholder="e.g: MAKOBERO Bertrand"></ion-input>
+        </ion-item>
       </ion-content>
-    </ion-header>
+    
   </ion-page>
 </template>
 
 <script>
-
 export default {
-}
+  
+};
 </script>
