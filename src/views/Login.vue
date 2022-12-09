aa392dfb58c096cf1048ec61e7ebc1b52f5c5ccd
Ber30 <makoberobertrand@gmail.com>
2022-12-09 20:04:21 +0200

    problem with login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,29 +3,46 @@
     <div class="container">
       <div class="centered">
         <ion-item>
-          <IonLabel>username</IonLabel>
+          <ion-label position="floating">username</ion-label>
           <Ion-Input type="text" />
         </ion-item>
         <ion-item>
-          <IonLabel>password</IonLabel>
+          <Ion-label position="floating">password</Ion-label>
           <Ion-input type="password" />
         </ion-item>
+        <ion-button expand="block" @click="seConnecter">connexion</ion-button>
       </div>
     </div>
   </ion-page>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    seConnecter() {
+      let logins = {
+        username: this.username,
+        password: this.password,
+      }
+      this.$store.state.user = logins
+    },
+  },
+};
 </script>
 <style scoped>
-.container{
+.container {
   display: flex;
   justify-content: center;
   background: rgb(44, 22, 63);
 }
 .centered {
   margin-top: 30%;
-  width: 30%;
+  width: 50%;
 }
 </style>
