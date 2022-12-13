208132a01e0878c169a82fa021c6837489e6cef4
Ber30 <makoberobertrand@gmail.com>
2022-12-13 19:40:15 +0200

    chorist registration done

--- Components/choriste.vue
+++ Components/choriste.vue
@@ -1,5 +1,18 @@
 <template>
+    <ion-item-sliding>
+        <ion-item-options side="start">
+          <ion-item-option color="success">Archive</ion-item-option>
+        </ion-item-options>
     
+        <ion-item>
+          <ion-label>Sliding Item with Options on Both Sides</ion-label>
+        </ion-item>
+    
+        <ion-item-options side="end">
+          <ion-item-option>Favorite</ion-item-option>
+          <ion-item-option color="danger">Delete</ion-item-option>
+        </ion-item-options>
+      </ion-item-sliding>
 </template>
 <script>
 export default {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,24 +2,14 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>MY FIRST IONIC APP</ion-title>
+        <ion-title>CHORISTS</ion-title>
       </ion-toolbar>
-      </ion-header>
-     <ion-content class="ion-padding">
-        <ion-item>
-          <ion-label position="floating">Your name</ion-label>
-          <ion-input clear-input="true"></ion-input>
-          <ion-label position="floating">Your password</ion-label>
-          <ion-input clear-input="true"></ion-input>
-        </ion-item>
-        <ion-button>Validate</ion-button>
-      </ion-content>
-    
+    </ion-header>
+    <ion-content><ion-list>
+    </ion-list></ion-content>
   </ion-page>
 </template>
 
 <script>
-export default {
-  
-};
+export default {};
 </script>
